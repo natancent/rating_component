352536b4940d0702a8cd45a021f4ae9c27427f67
Natan Vicente <natancent1@outlook.com>
2024-02-20 03:07:10 -0300

    Modify circle color onclick

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
     let circles = document.querySelectorAll('.circle');
-    circles.forEach(function(circle){
-       
-        circle.addEventListener('click',function(){
+
+    circles.forEach(function (circle) {
+        circle.addEventListener('click', function () {
             let voto = this.id;
             let answer = document.getElementById('answer');
-            answer.innerHTML=(`You selected ${voto} out of 5`)
-            this.style.backgroundColor = 'gray';
+            answer.innerHTML = (`You selected ${voto} out of 5`);
+
+           
+            circles.forEach(function (circle) {
+                circle.style.backgroundColor = ''; 
+            });
+
             
-    
-        }) 
-    })
+            this.style.backgroundColor = 'gray';
+        });
+    });
 });
-function showResult(){
+
+function showResult() {
     let result = document.getElementById('tks');
-    let vote = document.getElementById('vote')
-    result.style.display= 'flex';
-    vote.style.display='none';
+    let vote = document.getElementById('vote');
+    result.style.display = 'flex';
+    vote.style.display = 'none';
 }
